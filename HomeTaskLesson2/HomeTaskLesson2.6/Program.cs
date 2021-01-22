1b5a4229ec8a7c3945d28409fe91bdcd96b832c9
yaNimotsi <yanimotsi@yanmdex.ru>
2021-01-22 12:20:27 +0300

    Add task6

---
+++ HomeTaskLesson2/HomeTaskLesson2.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeTaskLesson2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maskAllDay = 0B_1111100;
+           
+            int maskOffice1 = 0B_1101000;
+            int maskOffice2 = 0B_0010111;
+            int maskOffice3 = 0B_1111111;
+
+            int maskWorksOffcie1 = maskOffice1 & maskAllDay;
+            int maskWorksOffcie2 = maskOffice2 & maskAllDay;
+            int maskWorksOffcie3 = maskOffice3 & maskAllDay;
+
+            Console.WriteLine(maskWorksOffcie1 == maskOffice1);
+            Console.WriteLine(maskWorksOffcie2 == maskOffice2);
+            Console.WriteLine(maskWorksOffcie3 == maskOffice3);
+
+            Console.ReadLine();
+        }
+    }
+}
