d085a025085e52429cd1e4397e64531b9d934047
yaNimotsi <yanimotsi@yanmdex.ru>
2021-01-24 21:06:01 +0300

    Correct code after review teacher

--- HomeTaskLesson2/HomeTaskLesson2.6/Program.cs
+++ HomeTaskLesson2/HomeTaskLesson2.6/Program.cs
@@ -4,21 +4,39 @@ namespace HomeTaskLesson2._6
 {
     class Program
     {
+        public enum MasksOffice
+        {
+            maskAllDay = 0B_1111100,
+            maskOffice1 = 0B_1101000,
+            maskOffice2 = 0B_0010111,
+            maskOffice3 = 0B_1111111
+        }
         static void Main(string[] args)
         {
-            int maskAllDay = 0B_1111100;
-           
-            int maskOffice1 = 0B_1101000;
-            int maskOffice2 = 0B_0010111;
-            int maskOffice3 = 0B_1111111;
+            Console.WriteLine("Введите номер офиса (1-3)");
+            int numOffice = Convert.ToInt32(Console.ReadLine());
 
-            int maskWorksOffcie1 = maskOffice1 & maskAllDay;
-            int maskWorksOffcie2 = maskOffice2 & maskAllDay;
-            int maskWorksOffcie3 = maskOffice3 & maskAllDay;
+            int maskWorkOffice;
 
-            Console.WriteLine(maskWorksOffcie1 == maskOffice1);
-            Console.WriteLine(maskWorksOffcie2 == maskOffice2);
-            Console.WriteLine(maskWorksOffcie3 == maskOffice3);
+            switch (numOffice)
+            {
+                case 1:
+                    maskWorkOffice = (byte)MasksOffice.maskAllDay & (byte)MasksOffice.maskOffice1;
+                    Console.WriteLine($"Флаг работы первого офиса: " + ((byte)maskWorkOffice == (byte)MasksOffice.maskOffice1));
+                    break;
+                case 2:
+                    maskWorkOffice = (byte)MasksOffice.maskAllDay & (byte)MasksOffice.maskOffice2;
+                    Console.WriteLine($"Флаг работы первого офиса: " + ((byte)maskWorkOffice == (byte)MasksOffice.maskOffice2));
+                    break;
+                case 3:
+                    maskWorkOffice = (byte)MasksOffice.maskAllDay & (byte)MasksOffice.maskOffice3 ;
+                    Console.WriteLine($"Флаг работы первого офиса: " + ((byte)maskWorkOffice == (byte)MasksOffice.maskOffice3));
+                    break;
+                default:
+                    Console.WriteLine("Введенное вами значение не допустимо");
+                    break;
+            }
+
 
             Console.ReadLine();
         }
